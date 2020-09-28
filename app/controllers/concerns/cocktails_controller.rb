4fecd62d2977b16cab34e3398bc6f62757a2963c
mickael joseph <joseph.mickk@gmail.com>
2020-09-28 17:31:23 +0200

    show et new

--- app/controllers/concerns/cocktails_controller.rb
+++ app/controllers/concerns/cocktails_controller.rb
@@ -21,10 +21,27 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    if @cocktail.update(coktail_params)
+      redirect_to cocktail_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    if @cocktail.destroy
+    redirect_to root_path
+    end
+  end
+
+
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :description)
   end
 
 end
