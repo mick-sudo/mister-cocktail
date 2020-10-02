707d7ce474839afba78b23a90eddd9cf3864a91f
mickael joseph <joseph.mickk@gmail.com>
2020-10-02 10:08:31 +0200

    reviewend

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,3 +1,4 @@
 class Cocktail < ApplicationRecord
   has_one_attached :photo
+  has_many :reviews, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
-  resources :cocktails
+  resources :cocktails do
+    resources :reviews, only: [ :new, :create ]
+  end
+  resources :reviews, only: [ :destroy ]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
